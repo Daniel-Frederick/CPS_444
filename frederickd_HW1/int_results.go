d9a4410c10ccaab4c61a75b44a6ae9541bbe88e1
Daniel Frederick <frederickd5@udayton.edu>
2025-09-09 15:48:31 -0400

    another assignment start

---
+++ frederickd_HW1/int_results.go
@@ -0,0 +1,18 @@
+package main
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	if (len(os.Args) < 2) {
+		fmt.Println("No Name given!")
+		return;
+	}
+	if (len(os.Args) > 3) {
+	  fmt.Println("Only enter one name!")
+		return;
+	}
+
+
+}
